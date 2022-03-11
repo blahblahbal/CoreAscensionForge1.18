7349b7a3b8b7bac2b396b06257b4b5b153817047
Brian Hansen <jabawakijadehawk@gmail.com>
2022-03-11 12:08:39 -0800

    Move some things around

--- src/main/java/net/blahblahbal/coreascent/item/ModTiers.java
+++ src/main/java/net/blahblahbal/coreascent/item/ModTiers.java
@@ -7,7 +7,10 @@ import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.common.ForgeTier;
+import net.minecraftforge.common.Tags;
+import net.minecraftforge.fml.common.Mod;
 
+@Mod.EventBusSubscriber(bus= Mod.EventBusSubscriber.Bus.MOD)
 public class ModTiers
 {
     public static final Tag.Named<Block> NEEDS_NETHERITE_TOOL =
@@ -15,7 +18,7 @@ public class ModTiers
     public static final Tag.Named<Block> NEEDS_BEDROCK_TOOL =
             BlockTags.createOptional(new ResourceLocation("forge:needs_bedrock_tool"));
     public static final ForgeTier TADANITE = new ForgeTier(5, 4000, 14f,
-            6f, 20, NEEDS_NETHERITE_TOOL,
+            6f, 20, Tags.Blocks.NEEDS_NETHERITE_TOOL,
             () -> Ingredient.of(ModItems.TADANITE_CRYSTAL.get()));
     public static final ForgeTier LUMITE = new ForgeTier(6, 4500, 15f,
             9f, 25, NEEDS_BEDROCK_TOOL,
